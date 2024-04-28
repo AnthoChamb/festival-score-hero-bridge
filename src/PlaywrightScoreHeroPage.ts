2c5c0e59882d4ebb4f64ca2ac497e727284530db
Anthony Chamberland <anthochamberland19@outlook.com>
2024-04-28 00:49:26 -0400

    Retrieve max score from Score Hero

--- src/PlaywrightScoreHeroPage.ts
+++ src/PlaywrightScoreHeroPage.ts
@@ -3,7 +3,8 @@ import { ScoreHeroPage } from "./interfaces/ScoreHeroPage";
 import { Game } from "./enums/Game";
 import { Platform } from "./enums/Platform";
 import { Score } from "./interfaces/Score";
-import { getInstrumentPrefix } from "./enums/Instrument";
+import { Instrument, getInstrumentPrefix } from "./enums/Instrument";
+import { Difficulty } from "./enums/Difficulty";
 
 export class PlaywrightScoreHeroPage implements ScoreHeroPage, AsyncDisposable {
     private readonly baseUrl = new URL('https://rockband.scorehero.com');
@@ -22,12 +23,42 @@ export class PlaywrightScoreHeroPage implements ScoreHeroPage, AsyncDisposable {
         await this.page.waitForURL(this.baseUrl.toString());
     }
 
+    getMaxScore = async (song: number, instrument: Instrument, difficulty: Difficulty) => {
+        const url = new URL('view_scores.php', this.baseUrl);
+        url.searchParams.set('game', Game.Festival.toString());
+        url.searchParams.set('platform', Platform.Any.toString());
+        url.searchParams.set('size', '1');
+        url.searchParams.set('group', instrument.toString());
+        url.searchParams.set('diff', difficulty.toString());
+        url.searchParams.set('song', song.toString());
+
+        await this.page.goto(url.toString());
+
+        const scoreElements = await this.page.evaluate(() => {
+            // Score Hero does not have any id or class for the scores table so we use this selector instead
+            return document.querySelectorAll<HTMLTableCellElement>(
+                "#content-popup > table > tbody > tr:nth-child(3) > td > table > tbody > tr:nth-child(1) > td > table:nth-child(7) > tbody > tr:not(.headrow, .browncol) > td:nth-child(2)"
+            );
+        });
+
+        let maxScore = 0;
+
+        for (const scoreElement of scoreElements.values()) {
+            const score = Number(scoreElement.innerText);
+            if (score > maxScore) {
+                maxScore = score;
+            }
+        }
+
+        return maxScore;
+    }
+
     insertScore = async (song: number, score: Score) => {
         const url = new URL('insert_score.php', this.baseUrl);
         url.searchParams.set('game', Game.Festival.toString());
         url.searchParams.set('platform', Platform.Any.toString());
         url.searchParams.set('size', '1');
-        url.searchParams.set('group',score.instrument.toString());
+        url.searchParams.set('group', score.instrument.toString());
         url.searchParams.set('diff', score.difficulty.toString());
         url.searchParams.set('song', song.toString());
         
--- src/interfaces/ScoreHeroPage.ts
+++ src/interfaces/ScoreHeroPage.ts
@@ -1,6 +1,9 @@
+import { Difficulty } from "../enums/Difficulty";
+import { Instrument } from "../enums/Instrument";
 import { Score } from "./Score";
 
 export interface ScoreHeroPage {
     login(username: string, password: string): Promise<void>;
+    getMaxScore(song: number, instrument: Instrument, difficulty: Difficulty): Promise<number>;
     insertScore(song: number, score: Score): Promise<void>;
 }
