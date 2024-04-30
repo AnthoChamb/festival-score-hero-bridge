af926687dbf97875ff9f5d058b268930c5c8d984
Anthony Chamberland <anthochamberland19@outlook.com>
2024-04-29 23:28:11 -0400

    Add command-line interface

--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,75 @@
+#!/usr/bin/env node
 import Database from 'better-sqlite3';
+import { Command } from 'commander';
 import * as fs from 'node:fs/promises';
 import { chromium } from 'playwright';
 import { FestivalScoreHeroBridge } from './FestivalScoreHeroBridge';
 import { PlaywrightScoreHeroPage } from './PlaywrightScoreHeroPage';
 import { SQLite3FestivalScoreProvider } from './SQLite3FestivalScoreProvider';
-import { ScoreHeroPage } from './interfaces/ScoreHeroPage';
 
-const synchSongs = async (scoreHeroPage: ScoreHeroPage, festivalScoreProvider: SQLite3FestivalScoreProvider) => {
-  const songs = await scoreHeroPage.getSongs();
-  festivalScoreProvider.insertSongs(songs);
-}
-
-const writeSongsFile = async (festivalScoreProvider: SQLite3FestivalScoreProvider) => {
-  const songs = festivalScoreProvider.getSongs();
-  const json = JSON.stringify(songs);
-  await fs.writeFile('score_hero_songs.json', json);
-}
-
-(async () => {
+const uploadHighScores = async (filename: string, username: string, password: string, comment?: string) => {
   await using browser = await chromium.launch();
   await using context = await browser.newContext();
   await using page = await context.newPage();
   await using scoreHeroPage = new PlaywrightScoreHeroPage(page);
-  await scoreHeroPage.login('', '');
+  await scoreHeroPage.login(username, password)
+  
+  const database = new Database(filename);
+  database.pragma('journal_mode = WAL');
+  const festivalScoreProvider = new SQLite3FestivalScoreProvider(database);
+  festivalScoreProvider.createSongsTable();
+  
+  const bridge = new FestivalScoreHeroBridge(festivalScoreProvider, scoreHeroPage);
+  await bridge.uploadHighScores(comment);
+}
 
-  const database = new Database('leaderboard_data.db');
+const synchSongs = async (filename: string) => {
+  await using browser = await chromium.launch();
+  await using context = await browser.newContext();
+  await using page = await context.newPage();
+  await using scoreHeroPage = new PlaywrightScoreHeroPage(page);
+  
+  const database = new Database(filename);
   database.pragma('journal_mode = WAL');
   const festivalScoreProvider = new SQLite3FestivalScoreProvider(database);
   festivalScoreProvider.createSongsTable();
 
-  const bridge = new FestivalScoreHeroBridge(festivalScoreProvider, scoreHeroPage);
-  await bridge.uploadHighScores();
+  const songs = await scoreHeroPage.getSongs();
+  festivalScoreProvider.insertSongs(songs);
+}
+
+const writeSongsFile = async (source: string, destination: string) => {
+  const database = new Database(source);
+  database.pragma('journal_mode = WAL');
+  const festivalScoreProvider = new SQLite3FestivalScoreProvider(database);
+  festivalScoreProvider.createSongsTable();
+
+  const songs = festivalScoreProvider.getSongs();
+  const json = JSON.stringify(songs);
+  await fs.writeFile(destination, json);
+}
+
+(async () => {
+  const program = new Command()
+
+  program.command('upload')
+    .description('upload Festival high scores to Score Hero')
+    .argument('<filename>', 'Festival score database filename')
+    .requiredOption('-u, --username <username>', 'Score Hero username')
+    .requiredOption('-p, --password <password>', 'Score Hero password')
+    .option('-c, --comment <comment>', 'score comment')
+    .action(uploadHighScores);
+
+  program.command('synch')
+    .description('synch Score Hero song IDs with the Festival score database')
+    .argument('<filename>', 'Festival score database filename')
+    .action(synchSongs);
+
+  program.command('write')
+    .description('write Score Hero song IDs from the Festival score database to a JSON file')
+    .argument('<filename>', 'Festival score database filename')
+    .argument('[destination]', 'destination JSON filename', 'score_hero_songs.json')
+    .action(writeSongsFile);
+
+  await program.parseAsync(process.argv);
 })();
